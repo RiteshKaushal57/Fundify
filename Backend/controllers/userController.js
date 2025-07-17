9a780e0d4256a582c28805dd2ef5dde24c3bf6f3
RiteshKaushal57 <riteshkaushal57@gmail.com>
2025-07-17 17:50:44 +0530

    vercel modifications

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -64,8 +64,8 @@ export const loginUser = async (req, res) => {
     );
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
--- Backend/index.js
+++ Backend/index.js
@@ -66,8 +66,8 @@ app.get(
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 3600000, // 1 hour
     });
     res.redirect(process.env.GOOGLE_REDIRECT_URL);
