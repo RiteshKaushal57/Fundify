21e91e4c04954db5bb16d62b98762c25159f2082
RiteshKaushal57 <riteshkaushal57@gmail.com>
2025-07-19 18:06:39 +0530

    v

--- Backend/index.js
+++ Backend/index.js
@@ -35,10 +35,7 @@ app.use("/advisor/queries", advisorQueryRouter);
 const PORT = process.env.PORT || 4000;
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
